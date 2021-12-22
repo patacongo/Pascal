a9944b5791b38692998fd7b62754c035206663f7
Gregory Nutt <gnutt@nuttx.org>
2021-12-22 05:55:10 -0600

    - Add more test files

---
+++ tests/src/012-filerecord.pas
@@ -0,0 +1,20 @@
+program fileRecord;
+type
+FileInfo =
+  record
+     name: string;
+     f:    text;
+  end;
+
+var
+   Files : array[1..3] of NameString;
+
+begin
+   Files[1].name := "File1.dat";
+   Files[2].name := "File2".dat;
+   Files[3].name := "File3".dat;
+
+   AssignFile(Files[1].f, Files[1].name);
+   AssignFile(Files[2].f, Files[2].name);
+   AssignFile(Files[3].f, Files[3].name);
+end
---
+++ tests/src/013-arrayrecord.pas
@@ -0,0 +1,30 @@
+program arrayRecord;
+type
+NameString =
+  record
+     title: packed array [1..50] of char;;
+     info:  packed array [1..50] of char;;
+  end;
+
+var
+   Names : array[1..3] of NameString;
+
+procedure printBook( var name : NameString );
+begin
+   writeln(name.tile ": " name.info);
+end;
+
+begin
+   Names[1].tile := "Name 1";
+   Names[1].info := "This is name 1";
+
+   Names[2].tile := "Name 2";
+   Names[2].info := "This is name 2";
+
+   Names[3].tile := "Name 3";
+   Names[3].info := "This is name 3";
+
+   printName(Names[1]);
+   printName(Names[2]);
+   printName(Names[3]);
+end
